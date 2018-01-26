3ddf5a7d91f6bf585c16bf20fc2b472eb1ca9e83
zhangzzqq <zhangqihappyzq@163.com>
2018-01-26 16:44:44 +0800

    把一些资源文件提交

---
+++ app/src/main/java/com/zq/meizi/bean/LoadingBean.java
@@ -0,0 +1,8 @@
+package com.zq.meizi.bean;
+
+/**
+ * Created by Meiji on 2017/6/8.
+ */
+
+public class LoadingBean {
+}
---
+++ app/src/main/java/com/zq/meizi/bean/LoadingEndBean.java
@@ -0,0 +1,8 @@
+package com.zq.meizi.bean;
+
+/**
+ * Created by Meiji on 2017/7/11.
+ */
+
+public class LoadingEndBean {
+}
